559711334388d9c9b1a91acae7264cf80808b7e9
NikolaySezonov <nikolay.sezonov@gmail.com>
2022-02-12 21:41:52 +0000

    Practice codes for week 2 lecture

---
+++ readName.py
@@ -0,0 +1,8 @@
+# Read in a name and print it out
+# Nikolay Sezonov
+
+name = input('Enter your name, please:')
+print ('Hello,' + name)
+
+age = input('What is your age ?:')
+print ('Your age is {}'.format(age))
