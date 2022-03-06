443973c40a1eae4e8922d94605fad253333560b1
NikolaySezonov <nikolay.sezonov@gmail.com>
2022-03-06 15:37:44 +0000

    week 4

---
+++ Week04-flow/Testif.py
@@ -0,0 +1,13 @@
+# Program to show some if statement
+# Author: Nikolay Sezonov
+
+if True:
+    # statements
+    print("condition is true")
+
+if 2 == 2:
+    print("yes the world is sane")
+if 2 != 2:
+    print("Ihope thats not true")  
+else:
+    print("2 is not equal to 2 is false")
